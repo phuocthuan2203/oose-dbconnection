483bac3fe3da4406fcd7238f2a3e65a4fdbe0e7e
Nguyen Phuoc Thuan <nguyenphuocthuan.work@gmail.com>
2025-02-23 16:01:40 +0700

    change the mysql password for security

--- DbConnection/src/main/java/org/example/dbconnection/DBUtil.java
+++ DbConnection/src/main/java/org/example/dbconnection/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/calcdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "Thuan-1234";
+    private static final String PASSWORD = "password";
 
     static {
         try {
